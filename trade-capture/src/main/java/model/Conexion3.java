792179c86ac7be4e22e0f466c18cac396b92aa2c
Alberto Sánchez <jaehoo@gmail.com>
2013-05-26 00:15:41 -0500

    Adding legacy files

---
+++ trade-capture/src/main/java/model/Conexion3.java
@@ -0,0 +1,54 @@
+/*
+ * Clase para el manejo de la conexion a la Base de Datos
+ */
+
+package app.model;
+
+
+import javax.sql.DataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+
+/**
+ *
+ * @author Alberto Sanchez Gonzalez
+ *
+ */
+public class Conexion3 implements Conexiones{
+
+    // sfl4j log
+    private static Logger logger;
+    
+    // SPRING
+    private JdbcTemplate jt= null;
+
+    public Conexion3(){
+
+        logger = LoggerFactory.getLogger(this.getClass().getName());
+        logger.info("[ {} ]", this.getClass().getName());
+    }
+
+    public DataSource getDataSource() {
+
+        logger.debug("Obtener DataSource...");
+         
+        return getJt().getDataSource();
+    }
+
+    /**
+     * @return the jt
+     */
+    public JdbcTemplate getJt() {
+        return jt;
+    }
+
+    /**
+     * @param jt the jt to set
+     */
+    public void setJt(JdbcTemplate jt) {
+        this.jt = jt;
+    }
+
+}
